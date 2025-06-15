033769afbfb902d4745b1ea48c59dccaa8d2e2a3
DavisDiad <40240480@esmad.ipp.pt>
2025-06-15 10:19:23 +0100

    .

--- scenes/main_menu/scripts/main_menu.gd
+++ scenes/main_menu/scripts/main_menu.gd
@@ -9,3 +9,9 @@ func _on_button_pressed() -> void:
 	Transition.transition()
 	await Transition.on_transition_finished
 	get_tree().change_scene_to_file("res://scenes/cutscene/cutscene_1.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	Transition.transition()
+	await Transition.on_transition_finished
+	get_tree().quit()
